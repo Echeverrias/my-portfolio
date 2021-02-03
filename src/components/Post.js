03abe04f6ea87f785d2ac251f2cd6e0c4a7cf3f5
Echeverrias <juanan.echeve@gmail.com>
2021-02-03 22:24:33 +0100

    Changes in post. Can't acces to post.mainImage.asset.url, post.mainImage seems to be undifenied. The id and the url have been get in the sanityClient fetch , into te useEffect hook

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,12 +5,14 @@ import {Link} from 'react-router-dom'
 
 function Post(){
 
-    const [posts, setPosts] = useState(null)
+    const [posts, setPosts] = useState([])
 
     useEffect(() => {
             sanityClient.fetch(`*[_type == "post"]{
                     title,
                     slug,
+                    "urlImage": mainImage.asset->url,
+                    "idImage": mainImage.asset->_id,
                     mainImage{
                         asset->{
                             _id,
@@ -19,7 +21,9 @@ function Post(){
                         alt
                     }
                 }`)
-                .then((data) => setPosts(data))
+                .then((data) => {
+                    setPosts(data);
+                })
                 .catch((error) => console.log(error))
     
     }, [])
@@ -31,17 +35,19 @@ function Post(){
                         <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
                         <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page blog posts</h2>
                         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-                            { posts && 
+                            { posts && posts.length > 0 && 
                                 posts.map((post, index) => (
-                                    <article>
+                                    <article key={index}>
+                                         <p hidden>{JSON.stringify(post)}</p>
+                                         <p hidden>{JSON.stringify(post.mainImage)}</p>
                                         <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                                             <span 
                                                 className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-600 hover:shadow-lg  hover:border-green-400"
-                                                key={index}
+                                                
                                             >
                                                 <img 
-                                                    src={post.mainImage.asset.url} 
-                                                    alt={post.mainImage.alt}
+                                                    src={post.urlImage} 
+                                                    alt={post?.mainImage?.alt}
                                                     className="w-full h-full rounded-r object-cover absolute"
                                                 />
                                                 <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
